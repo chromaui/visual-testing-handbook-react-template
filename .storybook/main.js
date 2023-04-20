2f3e7254ac06a47eb4d75a1eea4e4a802573cf45
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-04-20 15:21:24 +0100

    Storybook and Vite migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
+export default config;
