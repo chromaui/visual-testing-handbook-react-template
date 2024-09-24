540895cd42e9d799f49f1aae87240ef5c1cfa1f6
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2024-09-24 08:56:53 +0100

    Updates Storybook to latest (8.3)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,15 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  staticDirs: ["../public"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
   },
 };
 export default config;
